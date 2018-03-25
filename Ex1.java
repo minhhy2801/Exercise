3a0401e326623855a380dfecd4a904f6a9379d69
thanhtienhuynh <37269231+thanhtienhuynh@users.noreply.github.com>
2018-03-25 18:21:31 +0700

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,20 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Exercise 1- Input an integer n, the program will show the first n primes 
 package ex1;
 
 import java.util.Scanner;
-/**
- *
- * @author IMMORTALITY IPOS
- */
+
 public class Ex1 {
     public static int checkPrimes(int a) {
         int i, h = 0;
-        for(i = 1; i < a; i++){
-            
+        for(i = 1; i < a; i++){            
             if( a % i == 0){
                 h++;
             }
@@ -23,15 +15,9 @@ public class Ex1 {
             System.out.printf("%d\n",a);
             return 1;
         }
-        else return 0;
-        
-        
-}
-    /**
-     * @param args the command line arguments
-     */
+        else return 0;               
+}   
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         int i, j = 0, input = 0, t;
         System.out.print("Input a number: ");
@@ -59,9 +45,7 @@ public class Ex1 {
         for(i = 1; j < input; i++){
            if(Ex1.checkPrimes(i) == 1){
                j++;
-           }
-            
+           }           
         }
     }
-
 }
